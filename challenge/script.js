97978de01a28b8baec19f33956ecd06b0237e822
AdamAliG <electron.adam@proton.me>
2023-06-09 08:20:24 +0200

    finished challange

--- challenge/script.js
+++ challenge/script.js
@@ -1,7 +1,7 @@
 let cars = [
-  {name: 'Car 1', year: '2015', image: 'url_to_image_1', price: '10000'},
-  {name: 'Car 2', year: '2018', image: 'url_to_image_2', price: '20000'},
-  {name: 'Car 3', year: '2020', image: 'url_to_image_3', price: '30000'},
+  {name: 'Ford Mustang', year: '2015', image: 'https://www.cnet.com/a/img/resize/545333abf055de40051f2c843b6578f3311db797/hub/2018/10/16/80c9b57c-1572-43a7-89bc-49a818239c00/2018-ford-mustang-gt-60.jpg?auto=webp&width=1200', price: '10000'},
+  {name: 'BMW', year: '2018', image: 'https://stimg.cardekho.com/images/carexteriorimages/930x620/BMW/X7/9462/1673948689052/front-left-side-47.jpg', price: '20000'},
+  {name: 'Camaro', year: '2020', image: 'https://www.carchaseheroes.com/uploads/cars/img1/2017-Camaro-zl1-yellow-bumblebee-.jpg', price: '30000'},
   // add more car objects...
 ];
 
